03cb6584b80c32fa7f84e81b8fdf35f6d700643d
Jared <jradtkey@gmail.com>
2017-06-15 18:30:25 -0700

    Add files via upload

---
+++ billion.js
@@ -0,0 +1,27 @@
+var count = .01;
+
+for (var i = 0; i <= 30; i++) {
+  count*=2;
+}
+
+console.log(count);
+
+var amount = .01;
+var days = 0;
+
+while (amount < 10000) {
+  amount*=2;
+  days++;
+}
+
+console.log(days);
+
+var total = .01;
+var day = 0;
+
+while (total < 1000000000) {
+  total*=2;
+  day++;
+}
+
+console.log(day);
